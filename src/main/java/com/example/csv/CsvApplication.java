f38fd9800f9e4881cdeed97fb5ed39d730cbdc46
imenmzoughi <imenmzoughi1234@gmail.com>
2023-03-16 15:37:09 +0100

    dockercompose

--- src/main/java/com/example/csv/CsvApplication.java
+++ src/main/java/com/example/csv/CsvApplication.java
@@ -1,12 +1,17 @@
 package com.example.csv;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+
 public class CsvApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsvApplication.class, args);
