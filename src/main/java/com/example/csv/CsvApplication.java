c04cc7e58e43d141f5c787d29cdaa82a3a935bc2
imenmzoughi <imenmzoughi1234@gmail.com>
2023-03-21 14:58:39 +0100

    new commit

--- src/main/java/com/example/csv/CsvApplication.java
+++ src/main/java/com/example/csv/CsvApplication.java
@@ -1,8 +1,12 @@
 package com.example.csv;
 
+import com.example.csv.domain.Role;
+import com.example.csv.domain.UserRole;
+import com.example.csv.repositories.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,8 +17,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class CsvApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsvApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(CsvApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner (RoleRepository repo){
+		return args -> {
+			UserRole consultant= new UserRole(1l,Role.CONSULTANT,null);
+			repo.save(consultant);
+		};
+	};
 
 }
