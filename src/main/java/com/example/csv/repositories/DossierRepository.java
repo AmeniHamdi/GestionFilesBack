828bd7e4a975d5f2793768f3c097e2e5b7388700
imenmzoughi <imenmzoughi1234@gmail.com>
2023-05-10 14:47:34 +0100

    wip

--- src/main/java/com/example/csv/repositories/DossierRepository.java
+++ src/main/java/com/example/csv/repositories/DossierRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface DossierRepository extends JpaRepository<Dossier, Long>{
+    @Query("SELECT c.Produit, COUNT(c.Produit) FROM Contrat c GROUP BY c.Produit")
+    List<Object[]> countByPr();
 
 }
