2eb2e17a0335cbdf02163f5305f221953a10c7ef
AmeniHamdi <44673905+AmeniHamdi@users.noreply.github.com>
2023-05-13 10:29:57 +0100

    fixes

--- src/main/java/com/example/csv/CsvApplication.java
+++ src/main/java/com/example/csv/CsvApplication.java
@@ -1,6 +1,5 @@
 package com.example.csv;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableSwagger2
-@EnableBatchProcessing
 @SpringBootApplication
 public class CsvApplication {
 
